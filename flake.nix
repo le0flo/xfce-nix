d266f2f58e44605e98f7bf1df177cb9c4871e07c
Leonardo <noreply@leoflo.me>
2026-07-19 15:32:37 +0200

    init devShells

--- flake.nix
+++ flake.nix
@@ -1,8 +1,6 @@
 {
   description = "Xfce's development packages";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
   outputs = {self, nixpkgs, ...}@inputs: let
     perSystem = nixpkgs.lib.genAttrs [
       "x86_64-linux"
@@ -10,10 +8,10 @@
       "aarch64-linux"
     ];
 
-    callPackage = system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in pkgs.lib.callPackageWith (pkgs // customPkgs system);
+    callPackage = system: (pkgs system).lib.callPackageWith (pkgs system // customPkgs system);
 
+    pkgs = system: nixpkgs.legacyPackages.${system};
+    
     customPkgs = system: nixpkgs.lib.genAttrs
       (builtins.attrNames (builtins.readDir ./pkgs))
       (name: callPackage system ./pkgs/${name}/package.nix {});
@@ -21,5 +19,36 @@
     packages = perSystem (system: customPkgs system);
 
     overlays = perSystem (system: final: prev: customPkgs system);
+
+    devShells = perSystem (system: let
+      shellPkgs = (pkgs system).extend self.overlays.${system};
+    in {
+      xfwl4-dev = shellPkgs.mkShell {
+        packages = with shellPkgs; [
+          gettext
+          pkg-config
+          meson
+          xwayland
+          cargo
+          rustc
+        ];
+
+        buildInputs = with shellPkgs; [
+          libdisplay-info
+          libdrm
+          libgbm
+          gtk3
+          libinput
+          pixman
+          seatd
+          udev
+          libxfce4ui
+          xfconf
+          libxkbcommon
+        ];
+      };
+    });
   };
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 }
