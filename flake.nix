7f60b9dd730453a00ea03ed300cf379d85b40106
Leonardo <noreply@leoflo.me>
2026-07-21 00:21:04 +0200

    flake: piccoli fix

--- flake.nix
+++ flake.nix
@@ -2,17 +2,17 @@
   description = "Xfce's development packages";
 
   outputs = {self, nixpkgs, ...}@inputs: let
-    perSystem = nixpkgs.lib.genAttrs [
+    lib = nixpkgs.lib;
+
+    pkgs = system: nixpkgs.legacyPackages.${system};
+    callPackage = system: (pkgs system).lib.callPackageWith (pkgs system // customPkgs system);
+    perSystem = lib.genAttrs [
       "x86_64-linux"
       "i686-linux"
       "aarch64-linux"
     ];
 
-    callPackage = system: (pkgs system).lib.callPackageWith (pkgs system // customPkgs system);
-
-    pkgs = system: nixpkgs.legacyPackages.${system};
-    
-    customPkgs = system: nixpkgs.lib.genAttrs
+    customPkgs = system: lib.genAttrs
       (builtins.attrNames (builtins.readDir ./pkgs))
       (name: callPackage system ./pkgs/${name}/package.nix {});
   in {
@@ -20,11 +20,13 @@
 
     overlays = perSystem (system: final: prev: customPkgs system);
 
-    devShells = perSystem (system: nixpkgs.lib.genAttrs
+    devShells = perSystem (system: lib.genAttrs
       (builtins.filter
         (name: builtins.pathExists ./pkgs/${name}/shell.nix)
         (builtins.attrNames (builtins.readDir ./pkgs)))
-      (name: callPackage system ./pkgs/${name}/shell.nix { pkgs = (pkgs system).extend self.overlays.${system}; }));
+      (name: callPackage system ./pkgs/${name}/shell.nix {
+        pkgs = (pkgs system).extend self.overlays.${system};
+      }));
   };
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
